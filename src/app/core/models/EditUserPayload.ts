9a3a1f2f5f8f4f9689091afd49b4f23632ad8664
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2022-09-26 01:04:36 -0300

    Criando métodos serviço User

---
+++ src/app/core/models/EditUserPayload.ts
@@ -0,0 +1,5 @@
+export interface EditUserPayload {
+  id: number;
+  name: string;
+  email: string;
+}
--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { delay, Observable } from 'rxjs';
 import endpoints from 'src/environments/endpoints';
+import { EditUserPayload } from '../../models/EditUserPayload';
 import { User } from '../../models/User';
 
 @Injectable({
@@ -14,5 +15,22 @@ export class UserService {
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.baseURL).pipe(delay(500));
+  }
+
+  getUser(id: number): Observable<User> {
+    const url = `${this.baseURL}/${id}`;
+
+    return this.http.get<User>(url).pipe(delay(500));
+  }
+  editUser(id: number, payload: EditUserPayload): Observable<User> {
+    const url = `${this.baseURL}/${id}`;
+
+    return this.http.put<User>(url, payload).pipe(delay(500));
+  }
+
+  deleteUser(id: number): Observable<void> {
+    const url = `${this.baseURL}/${id}`;
+
+    return this.http.delete<void>(url).pipe(delay(500));
   }
 }
